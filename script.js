c5ebc57d674e95d976dc4f9416eb86eb8d6595d6
Frahyang <frahyang2005@gmail.com>
2025-06-06 09:14:46 +0700

    added search filter function for showcase page

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ document.addEventListener('DOMContentLoaded', () => {
         hamburger.classList.toggle('active');
     });
   });
+
+// Function for project search filter
+function searchProject() {
+    var input, filter, projectList, projectItem, char, i, charValue;
+    input = document.getElementById("search-input");
+    filter = input.value.toUpperCase();
+    projectList = document.getElementById("project-list");
+    projectItem = projectList.getElementsByTagName("li");
+    for (i = 0; i < projectItem.length; i++) {
+        char = projectItem[i].getElementsByClassName("project-title")[0];
+        charValue = char.textContent || char.innerText;
+        if (charValue.toUpperCase().indexOf(filter) > -1) {
+            projectItem[i].style.display = "";
+        } else {
+            projectItem[i].style.display = "none";
+        }
+    }
+}
